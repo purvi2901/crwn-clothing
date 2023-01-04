31806674c622cb91b458de061d1955f5f41f2c2b
purvi2901 <purviparmar4269@gmail.com>
2023-01-04 11:30:56 +0530

    routesaddede

--- src/components/menuitems/menuitem-component.jsx
+++ src/components/menuitems/menuitem-component.jsx
@@ -1,10 +1,11 @@
  import React from 'react';
+import { matchPath } from 'react-router-dom';
  import './menuitem-style.scss';
 
 const Menuitem = (props) => {
   return (
-    <div className={`${props.size} menu-item `}>
-    <div className='background-image' style={{backgroundImage:`url(${props.image})`}} />
+    <div className={`${props.size} menu-item `} onClick={()=>history.push(`${matchRoutes}${linkUrl}`)}>
+    <div className='background-image' style={{backgroundImage:`url(${props.imageUrl})`}} />
     <div className="content">
       <h1 className="title">{props.title.toUpperCase()}</h1>
       <span className="subtitle">SHOPNOW</span>
@@ -14,4 +15,4 @@ const Menuitem = (props) => {
   )
 }
 
-export default Menuitem
+export default Menuitem;
