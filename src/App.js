c942c2bd9b30cc8246dba4661a9d497943065d0e
purvi2901 <purviparmar4269@gmail.com>
2023-01-04 11:36:56 +0530

    source file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage-component';
 import './pages/homepage/homepage-style.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+const HatsPage = () => {
+  return (
+    <div>HatsPage</div>
+  )
+}
+
+
+
+
+//add something
 
 function App() {
   return (
     <div className="App">
-       <div><HomePage/></div>
+      {/* <div><HomePage/></div> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/hats" element={<HatsPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
@@ -40,7 +59,7 @@ export default App;
 
 
 
-      {/* <header className="App-header">
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
